8f94b00c23a930dcb0bbf22c6a298c5680a18db4
TQAT0707GW <150802332+TQAT0707GW@users.noreply.github.com>
2025-05-18 10:45:18 +0700

    Update tb_dual_ram.sv

--- Dual_Port_Ram/tb_dual_ram.sv
+++ Dual_Port_Ram/tb_dual_ram.sv
@@ -51,16 +51,6 @@ module tb_dual_ram;
     initial begin
         RamTest test;
         test = new();
-
-        // Reset ban đầu
-        ram_if_inst.wea = 0;
-        ram_if_inst.web = 0;
-        ram_if_inst.addra = 0;
-        ram_if_inst.addrb = 0;
-        ram_if_inst.dina = 0;
-        ram_if_inst.dinb = 0;
-
-		
         // Chạy 10 trường hợp kiểm tra ngẫu nhiên
         repeat (5) begin
             assert(test.randomize()) else $fatal("Tạo dữ liệu ngẫu nhiên thất bại!");
